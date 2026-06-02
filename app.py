85562a705a94a967420e688f0414c149686a6632
eduardo marques dos santos <edumsantos2006@gmail.com>
2026-06-02 20:25:55 -0300

    finalização checkpoint 5

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, request
+from datetime import datetime
 import requests
 
 app = Flask(__name__)
 
 api_key = "bc8f00971e40640add44ac54e5d342f7"
+
+hora = datetime.now().strftime("%H:%M")
 
 
 @app.route('/', methods=["GET", "POST"])
@@ -11,19 +14,21 @@ def index():
     clima = None
     erro = None
     if request.method =="POST":
-        cidade = request.form.get("cidade")
+        cidade = request.form.get("cidade", "").strip()
         if cidade:
             url = f"https://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={api_key}&units=metric&lang=pt_br"
             try:
                 res = requests.get(url)
                 if res.status_code == 200:
                     clima = res.json()
+                elif res.status_code == 404:
+                    erro = "cidade não encontrada"
                 else:
-                    erro = "cidade não encontrada!"
+                    erro = "Erro ao consultar a API"
             
             except requests.exceptions.RequestException:
                 erro = "Erro ao conectar ao serviço."
-    return render_template("index.html", clima=clima, erro=erro)
+    return render_template("index.html", clima=clima, erro=erro, hora=hora)
     
 
 if __name__ == "__main__":
